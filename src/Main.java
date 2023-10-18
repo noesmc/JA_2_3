f2716b694d970cba417e29efac4a9a8589140c82
noesmc <nikita_sinykovich@mail.ru>
2023-10-18 21:38:16 +0300

    JA_2_3

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score;
+        while (true) {
+            System.out.print("Please enter your score: ");
+            try {
+                score = Integer.parseInt(scanner.next());
+                if (score >= 70 && score <= 100) {
+                    System.out.println("Congrats! You’ve passed the test!");
+                    break;
+                } else if (score >= 0 && score < 70) {
+                    System.out.println("Sorry, you’ve failed the test.");
+                    break;
+                    }
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong input!");
+            }
+        }
+    }
+}
