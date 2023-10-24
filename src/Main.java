63b0ee293ce97fa2bb1946c1b864c0dd322f71f3
noesmc <nikita_sinykovich@mail.ru>
2023-10-24 07:06:58 +0300

    JA_2_3

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,15 @@ public class Main {
             System.out.print("Please enter your score: ");
             try {
                 score = Integer.parseInt(scanner.next());
-                if (score >= 70 && score <= 100) {
+                 if (score >= 70 && score <= 100) {
                     System.out.println("Congrats! You’ve passed the test!");
                     break;
-                } else if (score >= 0 && score < 70) {
+                    } else if (score >= 0 && score < 70) {
                     System.out.println("Sorry, you’ve failed the test.");
                     break;
-                    }
+                    } else {
+                     System.out.println("Enter a value from the range 0 to 100");
+                 }
             } catch (NumberFormatException e) {
                 System.out.println("Wrong input!");
             }
